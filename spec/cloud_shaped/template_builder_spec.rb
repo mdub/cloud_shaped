eb3c8584066fea2b62dcc168653a435d1536290f
Mike Williams <mdub@dogbiscuit.org>
2014-07-12 15:46:57 +1000

    Simplify.

--- spec/cloud_shaped/template_builder_spec.rb
+++ spec/cloud_shaped/template_builder_spec.rb
@@ -4,36 +4,29 @@ require 'cloud_shaped/template_builder'
 
 describe CloudShaped::TemplateBuilder do
 
-  def self.define_template_builder(&block)
-    let(:template_builder_class) do
-      Class.new(CloudShaped::TemplateBuilder, &block)
-    end
-  end
+  it "builds CloudFormation templates" do
 
-  context "with an empty #build method" do
-
-    define_template_builder do
+    template_builder_class = Class.new(CloudShaped::TemplateBuilder) do
 
       def build
       end
 
     end
 
-    it "builds an empty template" do
-      empty_template = {
-        "AWSTemplateFormatVersion" => '2010-09-09',
-        "Parameters" => {},
-        "Resources" => {},
-        "Outputs" => {}
-      }
-      expect(template_builder_class.build).to eq(empty_template)
-    end
+    empty_template = {
+      "AWSTemplateFormatVersion" => '2010-09-09',
+      "Parameters" => {},
+      "Resources" => {},
+      "Outputs" => {}
+    }
+
+    expect(template_builder_class.build).to eq(empty_template)
 
   end
 
-  context "with resources defined" do
+  it "supports resources" do
 
-    define_template_builder do
+    template_builder_class = Class.new(CloudShaped::TemplateBuilder) do
 
       def build
         resources["fooBar"] = resource("AWS::Foo::Bar", "foo" => "bar")
@@ -41,20 +34,19 @@ describe CloudShaped::TemplateBuilder do
 
     end
 
-    it "includes the resources" do
-      template_with_elb = {
-        "AWSTemplateFormatVersion" => '2010-09-09',
-        "Parameters" => {},
-        "Resources" => {
-          "fooBar" => {
-            "Type" => "AWS::Foo::Bar",
-            "Properties" => {"foo" => "bar"}
-          }
-        },
-        "Outputs" => {}
-      }
-      expect(template_builder_class.build).to eq(template_with_elb)
-    end
+    template_with_elb = {
+      "AWSTemplateFormatVersion" => '2010-09-09',
+      "Parameters" => {},
+      "Resources" => {
+        "fooBar" => {
+          "Type" => "AWS::Foo::Bar",
+          "Properties" => {"foo" => "bar"}
+        }
+      },
+      "Outputs" => {}
+    }
+
+    expect(template_builder_class.build).to eq(template_with_elb)
 
   end
 
