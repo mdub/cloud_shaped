2b3ca5e53614b4d6b0b3a1c7f4db7702047ad9c6
Mike Williams <mdub@dogbiscuit.org>
2014-07-14 12:30:02 +1000

    An example.

--- examples/buckets.rb
+++ examples/buckets.rb
@@ -3,15 +3,22 @@
 require 'cloud_shaped'
 require 'yaml'
 
-def make_buckets(n_buckets = 1)
-  CloudShaped.template do |t|
-    1.upto(n_buckets) do |i|
-      bucket_name = "buckety_#{i}"
-      t.def_resource "bucket#{i}", "AWS::S3::Bucket" do |b|
-        b["BucketName"] = "my-bucket-#{i}"
+class BucketMaker
+
+  def initialize(n_buckets = 1)
+    @n_buckets = n_buckets
+  end
+
+  def template
+    CloudShaped.template do |t|
+      1.upto(@n_buckets) do |i|
+        t.def_resource "bucket#{i}", "AWS::S3::Bucket" do |b|
+          b["BucketName"] = "my-bucket-#{i}"
+        end
       end
     end
   end
+
 end
 
-puts YAML.dump(make_buckets(3))
+puts YAML.dump(BucketMaker.new(3).template)
