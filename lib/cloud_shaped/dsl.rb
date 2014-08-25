692b4a0a337f0217884a56639c86fb536e8f9837
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-08-25 13:37:11 +0000

    lint

--- lib/cloud_shaped/dsl.rb
+++ lib/cloud_shaped/dsl.rb
@@ -3,13 +3,12 @@ require 'cloud_shaped/function_methods'
 require 'cloud_shaped/sns_methods'
 
 module CloudShaped
+  # Domain Specific Language methods for CF template generation
   module DSL
-
     include CoreMethods
     include FunctionMethods
     include SnsMethods
 
     extend self
-
   end
 end
