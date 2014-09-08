d04f0857ab5b158b6d0ab3501410de890aa5f04c
Kevin Littlejohn <kevin@littlejohn.id.au>
2014-09-08 06:26:48 +0000

    add support for mailto: on email address

--- lib/cloud_shaped/sns_methods.rb
+++ lib/cloud_shaped/sns_methods.rb
@@ -1,27 +1,28 @@
 require 'cloud_shaped/core_methods'
 
 module CloudShaped
-
+  # Methods to create sns topics
   module SnsMethods
-
     include CoreMethods
 
-    def sns_topic(target)
-      resource "AWS::SNS::Topic", "Subscription" => [
-        { "Protocol" => sns_protocol(target), "Endpoint" => target }
+    def sns_topic(endpoint)
+      proto, target = sns_proto_target(endpoint)
+      resource 'AWS::SNS::Topic', 'Subscription' => [
+        { 'Protocol' => proto, 'Endpoint' => target }
       ]
     end
 
     private
 
-    def sns_protocol(target)
+    def sns_proto_target(target)
       case target
       when /^(https?):/
-        $1.upcase
-      else "email"
+        [Regexp.last_match[1].upcase, target]
+      when /^(mailto):(.*)/
+        ['email', Regexp.last_match[2]]
+      else ['email', target]
       end
     end
-
   end
-
 end
+
