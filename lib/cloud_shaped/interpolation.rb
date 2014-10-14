947a4507e6c7bf0850131c251e8f99bbc7e192b9
Mike Williams <mdub@dogbiscuit.org>
2014-10-14 11:20:49 +1100

    Implement #interpolate.
    Should be useful for generating user-data scripts, and the like.

---
+++ lib/cloud_shaped/interpolation.rb
@@ -0,0 +1,42 @@
+require 'cloud_shaped/core_methods'
+require 'cloud_shaped/function_methods'
+
+module CloudShaped
+
+  module Interpolation
+
+    include CoreMethods
+    include FunctionMethods
+
+    DEFAULT_DELIMITERS =['{{','}}']
+
+    # Interpolates a String, inserting calls to "Ref" and "Fn::GetAtt".
+    #
+    # @param string [String] input string
+    # @param delimiters [Array] opening and closing delimter
+    #
+    def interpolate(string, delimiters = DEFAULT_DELIMITERS)
+      fragments = string.lines.map do |line|
+        interpolate_line(line, delimiters)
+      end.flatten
+      fn_join('', fragments)
+    end
+
+    private
+
+    def interpolate_line(line, delimiters)
+      open, close = delimiters
+      tokens = line.split(/(#{Regexp.quote(open)}\w+#{Regexp.quote(close)})/)
+      tokens.reject!(&:empty?)
+      tokens.map do |token|
+        if token =~ /^#{Regexp.quote(open)}([\w:]+)(?:\.(\w+))?#{Regexp.quote(close)}$/
+          ref($1, $2)
+        else
+          token
+        end
+      end
+    end
+
+  end
+
+end
---
+++ spec/cloud_shaped/interpolation_spec.rb
@@ -0,0 +1,106 @@
+require 'spec_helper'
+
+require 'cloud_shaped/interpolation'
+
+describe CloudShaped::Interpolation do
+
+  include described_class
+
+  describe "#interpolate" do
+
+    let(:output) { interpolate(input) }
+
+    context "with a resource or parameter name" do
+
+      let(:input) { "prefix-{{myResource}}-suffix" }
+
+      it "generates a Ref" do
+        expect(output).to eq(
+          {
+            "Fn::Join" => [
+              "", [
+                "prefix-",
+                { "Ref" => "myResource" },
+                "-suffix"
+              ]
+            ]
+          }
+        )
+      end
+
+    end
+
+    context "with a built-in CloudFormation resource" do
+
+      let(:input) { "{{AWS::StackName}}" }
+
+      it "generates a Ref" do
+        expect(output).to eq(
+          {
+            "Fn::Join" => [
+              "", [
+                { "Ref" => "AWS::StackName" }
+              ]
+            ]
+          }
+        )
+      end
+
+    end
+
+    context "with a resource name and attribute" do
+
+      let(:input) { "{{loadBalancer.cname}}" }
+
+      it "generates an Fn::GetAtt" do
+        expect(output).to eq(
+          {
+            "Fn::Join" => [
+              "", [
+                { "Fn::GetAtt" => ["loadBalancer", "cname"] },
+              ]
+            ]
+          }
+        )
+      end
+
+    end
+
+    it "supports alternate delimiters" do
+      double_square_brackets = ["[[", "]]"]
+      expect(interpolate("[[foo]]", double_square_brackets)).to eq(
+          {
+            "Fn::Join" => [
+              "", [
+                { "Ref" => "foo" }
+              ]
+            ]
+          }
+        )
+    end
+
+    context "with a mix of stuff" do
+
+      let(:input) { "#!/bin/foo\n\nprefix-{{myResource}}-suffix\n" }
+
+      it "all just works" do
+        expect(output).to eq(
+          {
+            "Fn::Join" => [
+              "", [
+                "#!/bin/foo\n",
+                "\n",
+                "prefix-",
+                { "Ref" => "myResource" },
+                "-suffix\n"
+              ]
+            ]
+          }
+        )
+      end
+
+    end
+
+  end
+
+end
